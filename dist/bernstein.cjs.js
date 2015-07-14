5d0c950dca607b8eb99b66efd0ee32b6ee17785b
Daniel Sellers <daniel@designfrontier.net>
2015-07-14 12:18:11 -0600

    forgot to build

--- dist/bernstein.cjs.js
+++ dist/bernstein.cjs.js
@@ -1,29 +1,36 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
 var create = function create(functions) {
     return function (data) {
 
-        return functions.reduce(function (prev, curr) {
-            return prev.then(function (request) {
-                return new Promise(function (resolve, reject) {
-                    var p = curr.apply(null, [request, data, resolve]);
-
-                    if (p instanceof Promise) {
-                        p.then(function (resp) {
-                            resolve(resp);
-                        });
-                    }
-                });
+        if (typeof functions !== 'object') {
+            return new Promise(function (res, rej) {
+                //immediate promise to start the stack off right
+                res(JSON.parse(JSON.stringify(data)));
             });
-        }, new Promise(function (res, rej) {
-            //immediate promise to start the stack off right
-            res(JSON.parse(JSON.stringify(data)));
-        }));
+        } else {
+            return functions.reduce(function (prev, curr) {
+                return prev.then(function (request) {
+                    return new Promise(function (resolve, reject) {
+                        var p = curr.apply(null, [request, data, resolve]);
+
+                        if (p instanceof Promise) {
+                            p.then(function (resp) {
+                                resolve(resp);
+                            });
+                        }
+                    });
+                });
+            }, new Promise(function (res, rej) {
+                //immediate promise to start the stack off right
+                res(JSON.parse(JSON.stringify(data)));
+            }));
+        }
     };
 };
 
-exports["default"] = { create: create };
-module.exports = exports["default"];
+exports['default'] = { create: create };
+module.exports = exports['default'];
