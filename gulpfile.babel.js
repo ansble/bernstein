776a328c69fa79145272d941788bc9203548bbbf
Daniel Sellers <daniel@designfrontier.net>
2015-07-14 07:29:53 -0600

    initial commit and this is now it's own module

---
+++ gulpfile.babel.js
@@ -0,0 +1,42 @@
+import babelRegister from 'babel/register';
+import gulp from 'gulp';
+import mocha from 'gulp-mocha';
+import sourceMaps from 'gulp-sourcemaps';
+import gutil from 'gulp-util';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import streamify from 'gulp-streamify';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import jshint from 'gulp-jshint';
+
+
+gulp.task('test', function(done){
+    return gulp.src(['*_test.js'], {read: false})
+            .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('dev', ['build'], () => {
+    gulp.watch(['**/*.js', '!app.js', '!**/*_test.js', '!gulpfile.js'], ['build']);
+});
+
+gulp.task('build', () => {
+    let bundler = browserify({ debug: true }),
+        b;
+
+    bundler.transform(babelify);
+    bundler.add('./index.js');
+
+    b = bundler.bundle()
+        .on('error', gutil.log)
+        .pipe(source('./index.js'))
+        .pipe(gulp.dest('../'))
+        .pipe(streamify(uglify()))
+        .pipe(rename('bernstein.min.js'))
+        .pipe(gulp.dest('./dist'));
+
+    return gulp.src('./index.js')
+            .pipe(rename('bernstein.es6.js'))
+            .pipe(gulp.dest('./dist'));
+});
---
+++ index_test.js
@@ -0,0 +1,55 @@
+import chai from 'chai';
+import * as stack from './index';
+
+let assert = chai.assert;
+
+describe('Stack tests', () => {
+
+    it('should be properly defined as an object', () => {
+        assert.isObject(stack);
+        assert.isFunction(stack.create);
+    });
+
+    it('shoult return a function when create is called', () => {
+        let funcArr = [(data, original, next)=>{
+                data.num ++;
+                next(data);
+            }
+            , (data, original, next) => {
+                data.num ++;
+                next(data);
+            }
+            , (data, original) => {
+                return new Promise(function (res, rej){
+                    data.num ++;
+                    res(data);
+                });
+            }];
+
+        assert.isFunction(stack.create(funcArr));
+    });
+
+    it('should execute each of an array of functions in order', (done) => {
+        let funcArr = [(data, original, next)=>{
+                data.num ++;
+                next(data);
+            }
+            , (data, original, next) => {
+                data.num ++;
+                next(data);
+            }
+            , (data, original) => {
+                return new Promise(function (res, rej){
+                    data.num ++;
+                    res(data);
+                });
+            }]
+
+            , run = stack.create(funcArr);
+
+        run({num: 1}).then(function (data){
+            assert.isTrue(data.num === 4);
+            done();
+        });
+    });
+});
