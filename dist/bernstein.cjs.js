603fb87c546ae5dd828cb63483f1685b4fae661c
Daniel Sellers <daniel@designfrontier.net>
2015-07-20 21:57:44 -0600

    rebuilt for release

--- dist/bernstein.cjs.js
+++ dist/bernstein.cjs.js
@@ -1,36 +1,21 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var create = function create(functions) {
+    functions = Array.isArray(functions) ? functions : [];
     return function (data) {
-
-        if (typeof functions !== 'object') {
-            return new Promise(function (res, rej) {
-                //immediate promise to start the stack off right
-                res(JSON.parse(JSON.stringify(data)));
-            });
-        } else {
-            return functions.reduce(function (prev, curr) {
-                return prev.then(function (request) {
-                    return new Promise(function (resolve, reject) {
-                        var p = curr.apply(null, [request, data, resolve]);
-
-                        if (p instanceof Promise) {
-                            p.then(function (resp) {
-                                resolve(resp);
-                            });
-                        }
-                    });
+        return functions.reduce(function (prev, curr) {
+            return prev.then(function (request) {
+                return new Promise(function (resolve, reject) {
+                    resolve(curr.apply(null, [request, data, resolve]));
                 });
-            }, new Promise(function (res, rej) {
-                //immediate promise to start the stack off right
-                res(JSON.parse(JSON.stringify(data)));
-            }));
-        }
+            });
+            //immediate promise to start the stack off right
+        }, Promise.resolve(JSON.parse(JSON.stringify(data))));
     };
 };
 
-exports['default'] = { create: create };
-module.exports = exports['default'];
+exports["default"] = { create: create };
+module.exports = exports["default"];
